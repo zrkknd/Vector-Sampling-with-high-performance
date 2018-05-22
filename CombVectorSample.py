a2849f4751eb6ee703b7561dbbfa657d37bbe98b
zrkknd <38313249+zrkknd@users.noreply.github.com>
2018-05-22 14:51:01 +0000

    Update CombVectorSample.py
    add GetComb(n,m,x) function

--- CombVectorSample.py
+++ CombVectorSample.py
@@ -1,3 +1,15 @@
 import random as rd
 from scipy.special import comb
 
+def GetComb(n,m,x):
+  """
+  make a combination vector
+  parameter:
+  n, complete number
+  m, combination number
+  x, vector position in vector complete set
+  return:
+  a vector as list.
+  """
+  ls = Position_recursion(n,m,x,[]) 
+  return [y-1 for y in ls]
