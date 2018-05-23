25cba39c3034cf8cac3219898c365662f1274cb6
zrkknd <38313249+zrkknd@users.noreply.github.com>
2018-05-23 07:36:52 +0000

    Update CombVectorSample.py

--- CombVectorSample.py
+++ CombVectorSample.py
@@ -1,9 +1,38 @@
-import random as rd
 from scipy.special import comb
 """
-use function GetCombVector(n,m,x) to get a combination vector
+use function CombVectorSample.GetCombVector(n,m,x) to get a combination vector
 """
-def make_combination_number_list(n,m):
+
+class CombVectorCalc():
+  """
+  A Class to calculate a combination vector by it's positon on complete combination set
+  """
+  def __init__(self):
+    self.vector = None
+    
+  def GetCombVector(self,n,m,x):
+    """  
+    make a combination vector  
+    parameters:    
+    n, complete number    
+    m, combination number    
+    x, vector position in vector complete set  
+    return:    
+    a vector as list.
+    Example: 
+        >>> cv_calc=CombVectorCalc()  
+        >>> cv_calc.GetCombVector(6,3,14)
+        [1,3,4] 
+    """ 
+    if isinstance(n,int) and isinstance(m,int) and isinstance(x,int):
+      if n <= 0 or m <= 0 or m > n or x <= 0 or x > comb(n,m):
+        raise ValueError
+      self.vector = []
+      self._number_selection_recursion(n,m,x)
+      return [y-1 for y in self.vector]
+    raise TypeError
+  
+def _make_combination_number_list(self,n,m):
   """
   make a list fill by combination numbers like comb(n-1,m-1),comb(n-2,m-1),...,comb(m-1,m-1)
   parameters:
@@ -19,54 +48,42 @@ def make_combination_number_list(n,m):
     ls.append(start)
    return ls
 
-def number_selection_recursion(n,m,x,ls):
+def _number_selection_recursion(self,n,m,x):
   """
   calculate the number selection of dimension by using recursion algorithm
   parameters:
     n, complete number
     m, combination number
     x, vector position on the complete set
-    ls, store dimension by a list
   return:
     ls, store dimension by a list
   """
   if m == 1:
     s = x
-    if len(ls) > 0:
-      s = ls[-1] + x
-      ls.append(s)
-      return ls
-  elif m == 0 or m > n or x > comb(n,m):
-    raise ValueError
-  comb_ls = make_combination_number_list(n,m)
+    if len(self.vector) > 0:
+      s = self.vector[-1] + x
+      self.vector.append(s)
+      return self.vector
+  comb_ls = self._make_combination_number_list(n,m)
   for i in range(len(comb_ls)):
     if x == comb_ls[i]:
       s = 0
-      if len(ls) > 0:
-        s = ls[-1]
-        ls.append(s + i)
+      if len(self.vector) > 0:
+        s = self.vector[-1]
+        self.vector.append(s + i)
         m -= 1
       for j in range(m,0,-1):
-        ls.append(n-j+1+s)
-        return ls
+        self.vector.append(n-j+1+s)
+        return self.vector
     elif x < comb_ls[i]:
       s = i
-      if len(ls) > 0:
-        s = ls[-1] + i
-        ls.append(s)
+      if len(self.vector) > 0:
+        s = self.vector[-1] + i
+        self.vector.append(s)
         break
-  ls = number_selection_recursion(n-i,m-1,x-comb_ls[i-1],ls)
-  return ls
-  
-def GetCombVector(n,m,x):
-  """
-  make a combination vector
-  parameters:
-    n, complete number
-    m, combination number
-    x, vector position in vector complete set
-  return:
-    a vector as list.
-  """
-  ls = number_selection_recursion(n,m,x,[]) 
-  return [y-1 for y in ls]
+  self._number_selection_recursion(n-i,m-1,x-comb_ls[i-1])
+  return self.vector
+
+class CombVectorSample():
+  def __init__(self):
+    
